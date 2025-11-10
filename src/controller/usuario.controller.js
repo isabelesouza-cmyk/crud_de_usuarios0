943e53b2f0cf46d1fbe2c6d4afbe3d6231a47eb4
Isabele <isabele.souza@aluno.faeterj-brm.faetec.rj.gov.br>
2025-11-10 19:24:33 -0300

    add 2 commits

---
+++ src/controller/usuario.controller.js
@@ -0,0 +1,55 @@
+import {response} from "express";
+import usuarioService from "../services/usuario.services.js";
+
+async function createUsuarioController(request, response){
+    const novoUsuario = request.body;
+
+    try {
+        const usuario = await usuarioService.createUsuarioService(novoUsuario);
+        response.status(201).send({usuario});
+    } catch(error) {
+        response.status(400).send(error.message);
+    }
+}
+
+async function findAllUsuarioController(request, response) {
+    try {
+        const usuario = await usuarioService.findAllUsuarioService();
+        response.status(200).send({usuario});
+    } catch(error) {
+        response.status(404).send(error.message);
+    }
+}
+
+async function findUsuarioByIdController(request, response) {
+
+    const {id} = request.params;
+
+    try {
+        const usuario = await usuarioService.findUsuarioByIdService(id);
+        response.status(200).send({usuario});
+    } catch(error) {
+        response.status(404).send(error.message);
+    }
+}
+
+async function updateUsuarioController(request, response) {
+    
+    const {id} = request.params;
+    const novoUsuario = request.body;
+
+    try {
+        const usuario = await produtoService.updateUsuarioService(id, novoUsuario);
+        response.status(201).send({usuario});
+    } catch(error) {
+        response.status(400).send(error.message);
+    }
+
+}
+
+export default {
+    createUsuarioController,
+    findAllUsuarioController,
+    findUsuarioByIdController,
+    updateUsuarioController
+}
---
+++ src/routes/usuario.routes.js
@@ -0,0 +1,11 @@
+import usuarioController from "../controller/usuario.controller.js";
+import {Router} from "express";
+
+const usuarioRouter = Router();
+
+usuarioRouter.post("/usuario", usuarioController.createUsuarioController);
+usuarioRouter.get("/usuario", usuarioController.findAllUsuarioController);
+usuarioRouter.get("/usuario/:id", usuarioController.findUsuarioByIdController);
+usuarioRouter.put("/usuario/:id", usuarioController.updateUsuarioController);
+
+export default usuarioRouter;
